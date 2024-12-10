1e4b2d0522fed568b42692896f12a66ccbb2560c
Mário Foganholi Fernandes <mfoganho@redhat.com>
2024-12-10 12:21:27 -0300

    [wip] apply suggestions

--- internal/controller/pipelinerun_controller.go
+++ internal/controller/pipelinerun_controller.go
@@ -35,6 +35,22 @@ const (
 	MaxSimultaneousPipelineRuns = 20
 )
 
+func countRunningPipelineRuns(existingPipelineRuns tektonv1beta1.PipelineRunList) (int, error) {
+	// Collect pipelineruns with state 'running' or 'started'
+	var runningPipelineRuns []*tektonv1beta1.PipelineRun
+	for i, pipelineRun := range existingPipelineRuns.Items {
+		if len(pipelineRun.Status.Conditions) > 0 &&
+			pipelineRun.Status.Conditions[0].Status == corev1.ConditionUnknown &&
+			(pipelineRun.Status.Conditions[0].Reason == tektonv1beta1.PipelineRunReasonRunning.String() ||
+				pipelineRun.Status.Conditions[0].Reason == tektonv1beta1.PipelineRunReasonStarted.String()) {
+			runningPipelineRuns = append(runningPipelineRuns, &existingPipelineRuns.Items[i])
+		}
+	}
+	numRunning := len(runningPipelineRuns)
+
+	return numRunning, nil
+}
+
 // PipelineRunReconciler reconciles a PipelineRun object
 type PipelineRunReconciler struct {
 	Client client.Client
@@ -59,28 +75,33 @@ func (r *PipelineRunReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	}
 
 	// Get pipelineruns
-	var childPipelineRuns tektonv1beta1.PipelineRunList
-	err := r.Client.List(ctx, &childPipelineRuns, client.InNamespace(req.Namespace))
+	var existingPipelineRuns tektonv1beta1.PipelineRunList
+	err := r.Client.List(ctx, &existingPipelineRuns, client.InNamespace(req.Namespace))
 	if err != nil {
 		log.Error(err, "Unable to list child pipelineruns")
 		return ctrl.Result{}, err
 	}
 
 	// Collect pipelineruns with state 'running' or 'started'
-	var runningPipelineRuns []*tektonv1beta1.PipelineRun
-	for i, pipelineRun := range childPipelineRuns.Items {
-		if len(pipelineRun.Status.Conditions) > 0 &&
-			pipelineRun.Status.Conditions[0].Status == corev1.ConditionUnknown &&
-			(pipelineRun.Status.Conditions[0].Reason == tektonv1beta1.PipelineRunReasonRunning.String() ||
-				pipelineRun.Status.Conditions[0].Reason == tektonv1beta1.PipelineRunReasonStarted.String()) {
-			runningPipelineRuns = append(runningPipelineRuns, &childPipelineRuns.Items[i])
-		}
+	// var runningPipelineRuns []*tektonv1beta1.PipelineRun
+	// for i, pipelineRun := range childPipelineRuns.Items {
+	// 	if len(pipelineRun.Status.Conditions) > 0 &&
+	// 		pipelineRun.Status.Conditions[0].Status == corev1.ConditionUnknown &&
+	// 		(pipelineRun.Status.Conditions[0].Reason == tektonv1beta1.PipelineRunReasonRunning.String() ||
+	// 			pipelineRun.Status.Conditions[0].Reason == tektonv1beta1.PipelineRunReasonStarted.String()) {
+	// 		runningPipelineRuns = append(runningPipelineRuns, &childPipelineRuns.Items[i])
+	// 	}
+	// }
+	// numRunning := len(runningPipelineRuns)
+	numRunning, err := countRunningPipelineRuns(existingPipelineRuns)
+	if err != nil {
+		log.Error(err, "Unable to count running pipelineruns")
+		return ctrl.Result{}, err
 	}
-	numRunning := len(runningPipelineRuns)
 
 	// Launch one pipelinerun if less than the maximum number is currently running
-	if numRunning < MaxSimultaneousPipelineRuns {
-		for _, pipelineRun := range childPipelineRuns.Items {
+	for numRunning < MaxSimultaneousPipelineRuns {
+		for _, pipelineRun := range existingPipelineRuns.Items {
 			if pipelineRun.IsPending() {
 				pipelineRun.Spec.Status = ""
 				err = r.Client.Update(ctx, &pipelineRun, &client.UpdateOptions{})
@@ -91,6 +112,11 @@ func (r *PipelineRunReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 				log.Info(fmt.Sprintf("\n\nPipelineRun is updated (pending state removed): %v", req.NamespacedName))
 			}
 		}
+		numRunning, err = countRunningPipelineRuns(existingPipelineRuns)
+		if err != nil {
+			log.Error(err, "Unable to count running pipelineruns")
+			return ctrl.Result{}, err
+		}
 	}
 
 	return ctrl.Result{}, nil
@@ -100,13 +126,9 @@ func (r *PipelineRunReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 func (r *PipelineRunReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&tektonv1beta1.PipelineRun{}).
-		/* TODO: For the time being we just ignore all types of events
-		In a second implementation we are going to listen to changes in the pipelinerun
-		and one of these events will have to return true, and the reconcile can be stuff
-		based on the event */
 		WithEventFilter(predicate.Funcs{
-			CreateFunc:  func(createEvent event.CreateEvent) bool { return true },
-			DeleteFunc:  func(deleteEvent event.DeleteEvent) bool { return true },
+			CreateFunc:  func(createEvent event.CreateEvent) bool { return false },
+			DeleteFunc:  func(deleteEvent event.DeleteEvent) bool { return false },
 			UpdateFunc:  func(updateEvent event.UpdateEvent) bool { return true },
 			GenericFunc: func(genericEvent event.GenericEvent) bool { return false },
 		}).
