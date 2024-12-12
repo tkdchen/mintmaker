5e43626a490c79215ea10fd2c8a6fd8f39266035
Qixiang Wan <qwan@redhat.com>
2024-12-12 15:21:11 +0800

    Add internal component package
    Add new internal component package that provides Go structs to
    encapsulate Konflux components and APIs for retrieving component
    information.

---
+++ internal/pkg/component/github/github.go
@@ -0,0 +1,261 @@
+package github
+
+import (
+	"context"
+	"fmt"
+	ghinstallation "github.com/bradleyfalzon/ghinstallation/v2"
+	"github.com/google/go-github/v45/github"
+	appstudiov1alpha1 "github.com/konflux-ci/application-api/api/v1alpha1"
+	"github.com/konflux-ci/mintmaker/internal/pkg/component/base"
+	"github.com/konflux-ci/mintmaker/internal/pkg/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"net/http"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+//TODO: doc about only supporting GitHub with the installed GitHub App
+
+var (
+	ghAppInstallationsCache          *Cache
+	ghAppInstallationsCacheMutex     sync.Mutex
+	ghAppInstallationsCacheID        int64
+	ghAppInstallationTokenCache      *Cache
+	ghAppInstallationTokenCacheMutex sync.Mutex
+	ghAppInstallationTokenCacheID    int64
+	ghAppID                          int64
+	ghAppPrivateKey                  []byte
+)
+
+type AppInstallation struct {
+	InstallationID int64
+	Repositories   []string
+}
+
+type Component struct {
+	base.BaseComponent
+	AppID         int64
+	AppPrivateKey []byte
+	client        client.Client
+	ctx           context.Context
+}
+
+func getAppIDAndKey(client client.Client, ctx context.Context) (int64, []byte, error) {
+	if ghAppID != 0 && ghAppPrivateKey != nil {
+		return ghAppID, ghAppPrivateKey, nil
+	}
+	//Check if GitHub Application is used, if not then skip
+	appSecret := corev1.Secret{}
+	appSecretKey := types.NamespacedName{Namespace: "mintmaker", Name: "pipelines-as-code-secret"}
+	if err := client.Get(ctx, appSecretKey, &appSecret); err != nil {
+		return 0, nil, err
+	}
+
+	// validate content of the fields
+	num, err := strconv.ParseInt(string(appSecret.Data["github-application-id"]), 10, 64)
+	if err != nil {
+		return 0, nil, fmt.Errorf("failed to parse GitHub APP ID: %w", err)
+	}
+	ghAppID = num
+	ghAppPrivateKey = appSecret.Data["github-private-key"]
+	return ghAppID, ghAppPrivateKey, nil
+}
+
+func NewComponent(comp *appstudiov1alpha1.Component, timestamp int64, client client.Client, ctx context.Context) (*Component, error) {
+	appID, appPrivateKey, err := getAppIDAndKey(client, ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get GitHub APP ID and private key: %w", err)
+	}
+	giturl := comp.Spec.Source.GitSource.URL
+	// TODO: a helper to validate and parse the git url
+	platform, err := utils.GetGitPlatform(giturl)
+	if err != nil {
+		return nil, err
+	}
+	host, err := utils.GetGitHost(giturl)
+	if err != nil {
+		return nil, err
+	}
+	repository, err := utils.GetGitPath(giturl)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Component{
+		BaseComponent: base.BaseComponent{
+			Name:        comp.Name,
+			Namespace:   comp.Namespace,
+			Application: comp.Spec.Application,
+			Platform:    platform,
+			Host:        host,
+			GitURL:      giturl,
+			Repository:  repository,
+			Timestamp:   timestamp,
+			Branch:      comp.Spec.Source.GitSource.Revision,
+		},
+		AppID:         appID,
+		AppPrivateKey: appPrivateKey,
+		client:        client,
+		ctx:           ctx,
+	}, nil
+}
+
+func (c *Component) GetBranch() (string, error) {
+	if c.Branch != "" {
+		return c.Branch, nil
+	}
+
+	branch, err := c.getDefaultBranch()
+	if err != nil {
+		// TODO, handle the error
+		return "main", nil
+	}
+	return branch, nil
+}
+
+func (c *Component) GetToken() (string, error) {
+	ghAppInstallationTokenCacheMutex.Lock()
+	defer ghAppInstallationTokenCacheMutex.Unlock()
+
+	if ghAppInstallationTokenCache == nil || ghAppInstallationTokenCacheID != c.Timestamp {
+		ghAppInstallationTokenCache = NewCache()
+		ghAppInstallationTokenCacheID = c.Timestamp
+	}
+
+	appInstallations, err := c.getAppInstallations()
+	if err != nil {
+		return "", fmt.Errorf("failed to get GitHub App installations: %w", err)
+	}
+
+	var installationID int64
+	found := false
+
+	for _, installation := range appInstallations {
+		for _, repo := range installation.Repositories {
+			repo = strings.TrimSuffix(strings.TrimPrefix(repo, "/"), "/")
+			if repo == c.Repository {
+				installationID = installation.InstallationID
+				found = true
+				break
+			}
+		}
+		if found {
+			break
+		}
+	}
+	if !found {
+		return "", fmt.Errorf("repository %s not found in any GitHub App installation", c.Repository)
+	}
+
+	token_key := fmt.Sprintf("installation_%d", installationID)
+	if token, ok := ghAppInstallationTokenCache.Get(token_key); ok {
+		return token.(string), nil
+	}
+
+	itr, err := ghinstallation.New(
+		http.DefaultTransport,
+		c.AppID,
+		installationID,
+		c.AppPrivateKey,
+	)
+	if err != nil {
+		return "", fmt.Errorf("error creating installation transport: %w", err)
+	}
+
+	token, err := itr.Token(context.Background())
+	if err != nil {
+		return "", fmt.Errorf("error getting installation token: %w", err)
+	}
+	ghAppInstallationTokenCache.Set(token_key, token)
+	return token, nil
+}
+
+func (c *Component) getAppInstallations() ([]AppInstallation, error) {
+	ghAppInstallationsCacheMutex.Lock()
+	defer ghAppInstallationsCacheMutex.Unlock()
+
+	if ghAppInstallationsCache == nil || ghAppInstallationsCacheID != c.Timestamp {
+		ghAppInstallationsCache = NewCache()
+		ghAppInstallationsCacheID = c.Timestamp
+	}
+	if data, ok := ghAppInstallationsCache.Get("installations"); ok {
+		return data.([]AppInstallation), nil
+	}
+
+	var appInstallations []AppInstallation
+
+	itr, err := ghinstallation.NewAppsTransport(http.DefaultTransport, c.AppID, c.AppPrivateKey)
+	if err != nil {
+		return nil, err
+	}
+
+	client := github.NewClient(&http.Client{Transport: itr})
+	_, _, err = client.Apps.Get(context.Background(), "")
+	if err != nil {
+		return nil, fmt.Errorf("failed to load GitHub app metadata, %w", err)
+	}
+
+	opt := &github.RepositoryListByOrgOptions{
+		ListOptions: github.ListOptions{PerPage: 100},
+	}
+	for {
+		installations, resp, err := client.Apps.ListInstallations(context.Background(), &opt.ListOptions)
+		if err != nil {
+			if resp != nil && resp.Response != nil && resp.Response.StatusCode != 0 {
+				switch resp.StatusCode {
+				case 401:
+					return nil, fmt.Errorf("GitHub Application private key does not match Application ID")
+				case 404:
+					return nil, fmt.Errorf("GitHub Application with given ID does not exist")
+				}
+			}
+			return nil, fmt.Errorf("error getting GitHub Application installations: %w", err)
+		}
+		for _, installation := range installations {
+			appInstall := AppInstallation{
+				InstallationID: installation.GetID(),
+			}
+
+			itr, err := ghinstallation.New(http.DefaultTransport, c.AppID, installation.GetID(), c.AppPrivateKey)
+			if err != nil {
+				return nil, fmt.Errorf("error creating installation transport: %w", err)
+			}
+
+			installationClient := github.NewClient(&http.Client{Transport: itr})
+			repoOpt := &github.ListOptions{PerPage: 100}
+			for {
+				repos, repoResp, err := installationClient.Apps.ListRepos(context.Background(), repoOpt)
+				if err != nil {
+					return nil, fmt.Errorf("error listing repos for installation %d: %w", installation.GetID(), err)
+				}
+				for _, repo := range repos.Repositories {
+					appInstall.Repositories = append(appInstall.Repositories, repo.GetFullName())
+				}
+				if repoResp.NextPage == 0 {
+					break
+				}
+				repoOpt.Page = repoResp.NextPage
+			}
+			appInstallations = append(appInstallations, appInstall)
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+
+	ghAppInstallationsCache.Set("installations", appInstallations)
+	return appInstallations, nil
+}
+
+func (c *Component) getDefaultBranch() (string, error) {
+	// TODO: call github APIs to determine the default branch
+	return "main", nil
+}
+
+func (c *Component) GetAPIEndpoint() string {
+	return fmt.Sprintf("https://api.%s/", c.Host)
+}
---
+++ internal/pkg/component/gitlab/gitlab.go
@@ -0,0 +1,167 @@
+package gitlab
+
+import (
+	"context"
+	"fmt"
+	appstudiov1alpha1 "github.com/konflux-ci/application-api/api/v1alpha1"
+	"github.com/konflux-ci/mintmaker/internal/pkg/component/base"
+	bslices "github.com/konflux-ci/mintmaker/internal/pkg/slices"
+	"github.com/konflux-ci/mintmaker/internal/pkg/utils"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/utils/strings/slices"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"strings"
+)
+
+//TODO: doc about only supporting GitHub with the installed GitHub App
+
+type Component struct {
+	base.BaseComponent
+	client client.Client
+	ctx    context.Context
+}
+
+func NewComponent(comp *appstudiov1alpha1.Component, timestamp int64, client client.Client, ctx context.Context) (*Component, error) {
+	giturl := comp.Spec.Source.GitSource.URL
+	// TODO: a helper to validate and parse the git url
+	platform, err := utils.GetGitPlatform(giturl)
+	if err != nil {
+		return nil, err
+	}
+	host, err := utils.GetGitHost(giturl)
+	if err != nil {
+		return nil, err
+	}
+	repository, err := utils.GetGitPath(giturl)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Component{
+		BaseComponent: base.BaseComponent{
+			Name:        comp.Name,
+			Namespace:   comp.Namespace,
+			Application: comp.Spec.Application,
+			Platform:    platform,
+			Host:        host,
+			GitURL:      giturl,
+			Repository:  repository,
+			Timestamp:   timestamp,
+			Branch:      comp.Spec.Source.GitSource.Revision,
+		},
+		client: client,
+		ctx:    ctx,
+	}, nil
+}
+
+func (c *Component) GetBranch() (string, error) {
+	if c.Branch != "" {
+		return c.Branch, nil
+	}
+
+	branch, err := c.getDefaultBranch()
+	if err != nil {
+		// TODO, handle the error
+		return "main", nil
+	}
+	return branch, nil
+}
+
+func (c *Component) lookupSecret() (*corev1.Secret, error) {
+
+	secretList := &corev1.SecretList{}
+	opts := client.ListOption(&client.MatchingLabels{
+		"appstudio.redhat.com/credentials": "scm",
+		"appstudio.redhat.com/scm.host":    c.Host,
+	})
+
+	// find secrets that have the following labels:
+	//	- "appstudio.redhat.com/credentials": "scm"
+	//	- "appstudio.redhat.com/scm.host": <name of component host>
+	if err := c.client.List(c.ctx, secretList, client.InNamespace(c.Namespace), opts); err != nil {
+		return nil, fmt.Errorf("failed to list scm secrets in namespace %s: %w", c.Namespace, err)
+	}
+
+	// filtering to get BasicAuth secrets and data is not empty
+	secrets := bslices.Filter(secretList.Items, func(secret corev1.Secret) bool {
+		return secret.Type == corev1.SecretTypeBasicAuth && len(secret.Data) > 0
+	})
+	if len(secrets) == 0 {
+		return nil, fmt.Errorf("no secrets available for git host %s", c.Host)
+	}
+
+	// secrets only match with component's host
+	var hostOnlySecrets []corev1.Secret
+	// map of secret index and its best path intersections count, i.e. the count of path parts matched,
+	var potentialMatches = make(map[int]int, len(secrets))
+
+	for index, secret := range secrets {
+		repositoryAnnotation, exists := secret.Annotations["appstudio.redhat.com/scm.repository"]
+		if !exists || repositoryAnnotation == "" {
+			hostOnlySecrets = append(hostOnlySecrets, secret)
+			continue
+		}
+
+		secretRepositories := strings.Split(repositoryAnnotation, ",")
+		// trim possible prefix or suffix "/"
+		for i, repository := range secretRepositories {
+			secretRepositories[i] = strings.TrimPrefix(strings.TrimSuffix(repository, "/"), "/")
+		}
+
+		// this secret matches exactly the component's repository name
+		if slices.Contains(secretRepositories, c.Repository) {
+			return &secret, nil
+		}
+
+		// no direct match, check for wildcard match, i.e. org/repo/* matches org/repo/foo, org/repo/bar, etc.
+		componentRepoParts := strings.Split(c.Repository, "/")
+
+		// find wildcard repositories
+		wildcardRepos := slices.Filter(nil, secretRepositories, func(s string) bool { return strings.HasSuffix(s, "*") })
+
+		for _, repo := range wildcardRepos {
+			i := bslices.Intersection(componentRepoParts, strings.Split(strings.TrimSuffix(repo, "*"), "/"))
+			if i > 0 && potentialMatches[index] < i {
+				// add whole secret index to potential matches
+				potentialMatches[index] = i
+			}
+		}
+	}
+
+	if len(potentialMatches) == 0 {
+		if len(hostOnlySecrets) == 0 {
+			// no potential matches, no host matches, nothing to return
+			return nil, fmt.Errorf("no secrets available for component")
+		}
+		// no potential matches, but we have host match secrets, return the first one
+		return &hostOnlySecrets[0], nil
+	}
+
+	// some potential matches exist, find the best one
+	var bestIndex, bestCount int
+	for i, count := range potentialMatches {
+		if count > bestCount {
+			bestCount = count
+			bestIndex = i
+		}
+	}
+	return &secrets[bestIndex], nil
+}
+
+func (c *Component) GetToken() (string, error) {
+
+	secret, err := c.lookupSecret()
+	if err != nil {
+		return "", err
+	}
+	return string(secret.Data[corev1.BasicAuthPasswordKey]), nil
+}
+
+func (c *Component) GetAPIEndpoint() string {
+	return fmt.Sprintf("https://%s/api/v4/", c.Host)
+}
+
+func (c *Component) getDefaultBranch() (string, error) {
+	// TODO: call github APIs to determine the default branch
+	return "main", nil
+}
